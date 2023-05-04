082eec2ec29e675152caad553ee52180620433a3
pepealacid <ppalacid@gmail.com>
2023-05-04 20:34:41 +0200

    angeles changes

--- project/public/scripts/record-audio.js
+++ project/public/scripts/record-audio.js
@@ -1,32 +1,29 @@
 let finalAudio;
-let mediaRecorder ;
+let mediaRecorder;
 const chunks = [];
 
 const startRecording = () => {
-  navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(stream => {
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
     mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.start();
-    mediaRecorder.addEventListener('dataavailable', e => {
-    chunks.push(e.data);
+    mediaRecorder.addEventListener("dataavailable", (e) => {
+      chunks.push(e.data);
     });
-   });
-  };
-      // stop recording and save audio
-      const stopRecording = () => {
-        mediaRecorder.stop();
-        mediaRecorder.addEventListener('stop', () => {
-          finalAudio = new Blob(chunks, { type: 'audio/webm' });
-          const audioPlayer = document.getElementById('audio-player');
-          audioPlayer.src = URL.createObjectURL(finalAudio);
-          console.log(URL.createObjectURL(finalAudio))
-        });
-      };
+  });
+};
+// stop recording and save audio
+const stopRecording = () => {
+  mediaRecorder.stop();
+  mediaRecorder.addEventListener("stop", () => {
+    finalAudio = new Blob(chunks, { type: "audio/webm" });
+    const audioPlayer = document.getElementById("audio-player");
+    audioPlayer.src = URL.createObjectURL(finalAudio);
+    console.log(URL.createObjectURL(finalAudio));
+  });
+};
 
-      // add event listeners to buttons
-      const startBtn = document.getElementById('start-btn');
-      const stopBtn = document.getElementById('stop-btn');
-      startBtn.addEventListener('click', startRecording);
-      stopBtn.addEventListener('click', stopRecording);
-
-
+// add event listeners to buttons
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+startBtn.addEventListener("click", startRecording);
+stopBtn.addEventListener("click", stopRecording);
